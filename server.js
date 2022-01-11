abe791ceeb31368cf9678699f6ed4dbae85491ec
SagarMedtiya <sagarmedtiya31@gmail.com>
2022-01-12 00:33:41 +0530

    add 404 route

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const Razorpay = require('razorpay')
 
 
 //Database connection
- const url = 'mongodb://localhost:27017/pizza';
+ const url = process.env.MONGODB;
  mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true
     }).then(()=>{
         console.log('Connection Successful');
@@ -64,6 +64,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 
 require('./routes/web')(app)
+app.use((req,res)=>{
+    res.status(404).send('<h1>404, page not found</h1>')
+})
 
 const server = app.listen(PORT,()=>{
                     console.log(`Listening to port ${PORT}`)
