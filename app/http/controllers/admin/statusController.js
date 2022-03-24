aa0118cd5de84d55b082f8c58d10086ee29ef1a6
SagarMedtiya <sagarmedtiya31@gmail.com>
2022-03-24 17:08:41 +0530

    update controller

--- app/http/controllers/admin/statusController.js
+++ app/http/controllers/admin/statusController.js
@@ -10,11 +10,8 @@ function statusController (){
                 //Emit Event
                 const eventEmitter = req.app.get('eventEmitter')
                 eventEmitter.emit('orderUpdated',{ id: req.body.orderId, status: req.body.status })
-
                 return res.redirect('/admin/order')
             })       
-        
-        
         }
     }
 }
